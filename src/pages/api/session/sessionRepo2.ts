cccd20f1b5e5526521152d474843ea1c70116a29
Danish Javed <dev.danish.javed@gmail.com>
2023-09-24 22:37:36 +0530

    Added logs

--- src/pages/api/session/sessionRepo2.ts
+++ src/pages/api/session/sessionRepo2.ts
@@ -4,13 +4,15 @@ const SessionRepo2 = () => {
 	async function saveSession(session:any){
 		const db = DBHelper().getDB();
 		const sessionDB = db.collection('sessionRep');
-		console.log('saving session to repo');
+		
 		try{
+			console.log('Saving Session', session.email, session.task);
 			const result = await sessionDB.insertOne(session);
+			console.log('Saved Successfully : ', result);
 			return result;
 		}
 		catch(e){
-			console.log('Something went wrong, saving session !!!',e);
+			console.log('Error Saving Session !!!',e);
 		}
 		
 	}
--- src/pages/api/user/userRepo2.ts
+++ src/pages/api/user/userRepo2.ts
@@ -1,13 +1,15 @@
 import { User } from "next-auth";
 import DBHelper from "../DBHelper";
+import { stdout } from "process";
 
 const UserRepo2 = ()=>{
 	const db = DBHelper().getDB();
 	const userCollection = db.collection('userRep');
 	async function saveUser(user:User){
-		console.log('saving user to DB');
+		console.log('Saving User To DB');
 		try{
 			const result = await userCollection.insertOne(user);
+			console.log('Saved successfully', result);
 		return result;
 		}
 		catch(e){
