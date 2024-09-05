aa54d69c855bb96934eb5db20e0ff285223ded38
Danish Javed <139096991+code-danish@users.noreply.github.com>
2024-09-05 10:13:11 +0530

    removed logs and commented code

--- src/pages/utility/submitSession.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-import { ISession } from "../api/session/sessionRepo";
-
-const submitSession = (session:ISession) =>{
-	axios.post('https://danish-oauth.vercel.app/api/session/save',session);
-}
-
-export default submitSession;
